3387fef0862e582f99491c0d4fc18159f35e4249
samflores23 <samuel.flores@steelseries.com>
2016-11-08 16:29:18 -0600

    Augmenting keyboard example with a consumer control. Also defaulting to keyboard type if checks fail when creating a device.

--- examples/keyboard.c
+++ examples/keyboard.c
@@ -4,6 +4,7 @@
  */
 
 #include <IOKit/IOKitLib.h>
+#include <IOKit/hid/IOHIDUsageTables.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,6 +13,7 @@ unsigned char report_descriptor[] = {
      0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
     0x09, 0x06,                    // USAGE (Keyboard)
     0xa1, 0x01,                    // COLLECTION (Application)
+    0x85, 0x01,                    //   REPORT_ID (1)
     0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
     0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
     0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
@@ -40,21 +42,43 @@ unsigned char report_descriptor[] = {
     0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
     0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
     0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
-    0x09, 0x00,                    //   USAGE (Reserved (no event indicated))
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x95, 0x01,                    //   REPORT_COUNT (1)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x7f,                    //   LOGICAL_MAXIMUM (127)
-    0xb1, 0x02,                    //   FEATURE (Data,Var,Abs)
-    0xc0                           // END_COLLECTION
+    0xc0,                          // END_COLLECTION
+
+    //
+    // CONSUMER CONTROL
+    //
+    0x05,0x0C,                          // USAGE_PAGE (Consumer Devices)
+    0x09,0x01,                          // USAGE (Consumer Control)
+    0xA1,0x01,                          // COLLECTION (Application)
+    0x85,0x02,                          //   REPORT_ID (2)
+    0x05,0x0C,                          //   USAGE_PAGE (Consumer Devices)
+    0x19,0x00,                          //   USAGE_MINIMUM (Unassigned)
+    0x2A,0x08,0x01,                     //   USAGE_MAXIMUM (Police Alarm)
+    0x15,0x00,                          //   LOGICAL_MINIMUM (0)
+    0x26,0x08,0x01,                     //   LOGICAL_MAXIMUM (264)
+    0x75,0x10,                          //   REPORT_SIZE (16)
+    0x95,0x02,                          //   REPORT_COUNT (2)
+    0x81,0x00,                          //   INPUT (Data,Ary,Abs)
+    0xC0                                // END_COLLECTION
 };
 
-struct keyboard_report_t {
-    uint8_t modifier;
+typedef struct keyboard_report_t {
+    uint8_t report_id;
+    uint8_t modifiers;
     uint8_t reserved;
     uint8_t key_codes[6];
-    uint8_t reserved_bis;
-};
+} keyboard_report_t;
+
+typedef struct consumer_report_t {
+    uint8_t report_id;
+    uint8_t key_codes[4];
+} consumer_report_t;
+
+
+typedef union composite_report_t {
+    keyboard_report_t keyboard;
+    consumer_report_t consumer;
+} composite_report_t;
 
 #define SERVICE_NAME "it_unbit_foohid"
 
@@ -114,24 +138,42 @@ int main() {
     }
 
     // Arguments to be passed through the HID message.
-    struct keyboard_report_t keyboard;
+    composite_report_t report;
+
     uint32_t send_count = 4;
     uint64_t send[send_count];
     send[0] = (uint64_t)input[0];  // device name
     send[1] = strlen((char *)input[0]);  // name length
-    send[2] = (uint64_t) &keyboard;  // keyboard struct
-    send[3] = sizeof(struct keyboard_report_t);  // keyboard struct len
+    send[2] = (uint64_t) &report;  // keyboard struct
+    send[3] = sizeof(composite_report_t);  // keyboard struct len
 
     int i = 0;
-    for(i = 0; ; i++) {
-        keyboard.modifier = 0;
+    for(i = 0; i < 5 ; i++) {
+        memset(&report, 0, sizeof(report));
+        report.keyboard.report_id = 0x01;
+        report.keyboard.key_codes[0] = kHIDUsage_KeyboardA;
 
-        if (i % 2 == 0) {
-            keyboard.key_codes[0] = 0x04;
-        } else {
-            keyboard.key_codes[0] = 0x00;
+        ret = IOConnectCallScalarMethod(connect, FOOHID_SEND, send, send_count, NULL, 0);
+        if (ret != KERN_SUCCESS) {
+            printf("Unable to send message to HID device.\n");
         }
 
+        report.keyboard.key_codes[0] = 0x00;
+        ret = IOConnectCallScalarMethod(connect, FOOHID_SEND, send, send_count, NULL, 0);
+        if (ret != KERN_SUCCESS) {
+            printf("Unable to send message to HID device.\n");
+        }
+
+        memset(&report, 0, sizeof(report));
+        report.consumer.report_id = 0x02;
+        report.consumer.key_codes[0] = kHIDUsage_Csmr_Mute;
+
+        ret = IOConnectCallScalarMethod(connect, FOOHID_SEND, send, send_count, NULL, 0);
+        if (ret != KERN_SUCCESS) {
+            printf("Unable to send message to HID device.\n");
+        }
+
+        report.consumer.key_codes[0] = 0x00;
         ret = IOConnectCallScalarMethod(connect, FOOHID_SEND, send, send_count, NULL, 0);
         if (ret != KERN_SUCCESS) {
             printf("Unable to send message to HID device.\n");
@@ -140,3 +182,4 @@ int main() {
         sleep(1);  // sleep for a second
     }
 }
+
