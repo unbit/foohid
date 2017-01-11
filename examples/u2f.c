446c010e9ca19ba9e6237d652ce439f8b3af3400
Ben Toews <mastahyeti@gmail.com>
2017-01-11 14:39:22 -0800

    makefile for examples (thanks @scottjg)

---
+++ examples/u2f.c
@@ -0,0 +1,94 @@
+/**
+ * Create a virtual keyboard.
+ * Compile me with: gcc keyboard.c -o virtual_keyboard -framework IOKit
+ */
+
+#include <IOKit/IOKitLib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned char report_descriptor[] = {
+  0x06, 0xD0, 0xF1,  // Usage Page (Reserved 0xF1D0)
+  0x09, 0x01,        // Usage (0x01)
+  0xA1, 0x01,        // Collection (Application)
+  0x09, 0x20,        //   Usage (0x20)
+  0x15, 0x00,        //   Logical Minimum (0)
+  0x26, 0xFF, 0x00,  //   Logical Maximum (255)
+  0x75, 0x08,        //   Report Size (8)
+  0x95, 0x40,        //   Report Count (64)
+  0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+  0x09, 0x21,        //   Usage (0x21)
+  0x15, 0x00,        //   Logical Minimum (0)
+  0x26, 0xFF, 0x00,  //   Logical Maximum (255)
+  0x75, 0x08,        //   Report Size (8)
+  0x95, 0x40,        //   Report Count (64)
+  0x91, 0x02,        //   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+  0xC0,              // End Collection
+};
+
+#define SERVICE_NAME "it_unbit_foohid"
+
+#define FOOHID_CREATE 0   // create selector
+#define FOOHID_DESTROY 1  // destroy selector
+#define FOOHID_SEND 2     // send selector
+#define FOOHID_LIST 3     // list selector
+
+#define DEVICE_NAME "Foohid Virtual U2F Device"
+#define DEVICE_SN "SN 123456"
+
+int main() {
+    io_iterator_t iterator;
+    io_service_t service;
+    io_connect_t connect;
+
+    // Get a reference to the IOService
+    kern_return_t ret = IOServiceGetMatchingServices(kIOMasterPortDefault, IOServiceMatching(SERVICE_NAME), &iterator);
+
+    if (ret != KERN_SUCCESS) {
+        printf("Unable to access IOService.\n");
+        exit(1);
+    }
+
+    // Iterate till success
+    int found = 0;
+    while ((service = IOIteratorNext(iterator)) != IO_OBJECT_NULL) {
+        ret = IOServiceOpen(service, mach_task_self(), 0, &connect);
+
+        if (ret == KERN_SUCCESS) {
+            found = 1;
+            break;
+        }
+
+        IOObjectRelease(service);
+    }
+    IOObjectRelease(iterator);
+
+    if (!found) {
+        printf("Unable to open IOService.\n");
+        exit(1);
+    }
+
+    // Fill up the input arguments.
+    uint32_t input_count = 8;
+    uint64_t input[input_count];
+    input[0] = (uint64_t) strdup(DEVICE_NAME);  // device name
+    input[1] = strlen((char *)input[0]);  // name length
+    input[2] = (uint64_t) report_descriptor;  // report descriptor
+    input[3] = sizeof(report_descriptor);  // report descriptor len
+    input[4] = (uint64_t) strdup(DEVICE_SN);  // serial number
+    input[5] = strlen((char *)input[4]);  // serial number len
+    input[6] = (uint64_t) 2;  // vendor ID
+    input[7] = (uint64_t) 3;  // device ID
+
+    printf("Creating HID device\n");
+    ret = IOConnectCallScalarMethod(connect, FOOHID_CREATE, input, input_count, NULL, 0);
+    if (ret != KERN_SUCCESS) {
+        printf("Unable to create HID device. May be fine if created previously.\n");
+    }
+
+    for(;;) {
+      sleep(30);
+      printf("foo\n");
+    }
+}
